8f26fee6fde1e0f6190ba192538c15cdb09706ff
Gerhard Weis <gerhard@weis.au.eu.org>
2009-10-22 02:30:09 +0000

    * adapted kml_openlayers.js to geosettings macros
    git-svn-id: http://svn.plone.org/svn/collective/collective.geo.kml/branches/geosettings-macros@100103 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/geo/kml/browser/javascripts/kml_openlayers.js
+++ collective/geo/kml/browser/javascripts/kml_openlayers.js
@@ -1,56 +1,64 @@
-var thisUri = window.location.href;
-thisUri = thisUri.slice(0, thisUri.length - window.location.hash.length);
 
-viewInUse = thisUri.match('@?@?kml-openlayers\/?$');
-if (viewInUse && viewInUse.length == 1) {
-      thisUri = thisUri.replace(viewInUse[0], "");
-}
 
-if (!thisUri.match('\/$')) {
-      thisUri += '/';
-}
+jq(document).ready(function() {
 
-kml = new OpenLayers.Layer.GML("KML Layer", thisUri + "@@kml-document",
-{
-    format: OpenLayers.Format.KML, 
-    projection: map.displayProjection,
-    formatOptions: {
-        extractStyles: true, 
-        extractAttributes: true
-    }
-})
+  var thisUri = window.location.href;
+  thisUri = thisUri.slice(0, thisUri.length - window.location.hash.length);
 
-map.addLayer(kml);
-var select = new OpenLayers.Control.SelectFeature(kml);
-    
-kml.events.on({
+  var viewInUse = thisUri.match('@?@?kml-openlayers\/?$');
+  if (viewInUse && viewInUse.length == 1) {
+    thisUri = thisUri.replace(viewInUse[0], "");
+  }
+
+  if (!thisUri.match('\/$')) {
+    thisUri += '/';
+  }
+
+  var map = cgmap.config['map'].map;
+
+  var kml = new OpenLayers.Layer.GML("KML Layer", thisUri + "@@kml-document",
+              {
+                format: OpenLayers.Format.KML,
+                projection: map.displayProjection,
+                formatOptions: {
+                  extractStyles: true,
+                  extractAttributes: true
+                }
+              });
+
+  map.addLayer(kml);
+  var select = new OpenLayers.Control.SelectFeature(kml);
+
+  kml.events.on({
     "featureselected": onFeatureSelect,
     "featureunselected": onFeatureUnselect
-});
+  });
 
-map.addControl(select);
-select.activate();
+  map.addControl(select);
+  select.activate();
 
-function onPopupClose(evt) {
+  function onPopupClose(evt) {
     select.unselectAll();
-}
-function onFeatureSelect(event) {
+  }
+  function onFeatureSelect(event) {
     var feature = event.feature;
     var selectedFeature = feature;
-    var popup = new OpenLayers.Popup.FramedCloud("chicken", 
-        feature.geometry.getBounds().getCenterLonLat(),
-        new OpenLayers.Size(200,200),
-        "<h2>"+feature.attributes.name + "</h2>" + feature.attributes.description,
-        null, true, onPopupClose
+    var popup = new OpenLayers.Popup.FramedCloud("chicken",
+                  feature.geometry.getBounds().getCenterLonLat(),
+                  new OpenLayers.Size(200,200),
+                  "<h2>"+feature.attributes.name + "</h2>" + feature.attributes.description,
+                  null, true, onPopupClose
     );
     feature.popup = popup;
     map.addPopup(popup);
-}
-function onFeatureUnselect(event) {
+  }
+  function onFeatureUnselect(event) {
     var feature = event.feature;
     if(feature.popup) {
-        map.removePopup(feature.popup);
-        feature.popup.destroy();
-        delete feature.popup;
+      map.removePopup(feature.popup);
+      feature.popup.destroy();
+      delete feature.popup;
     }
-}
+  }
+
+});
