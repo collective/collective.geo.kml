e702ac524765eec56290bc1457d268424f2979f1
David Beitey <david@davidjb.com>
2009-12-03 01:26:05 +0000

    Updating the KML javascript to be wrapped in a function call (needs to be called in window.load)
    git-svn-id: http://svn.plone.org/svn/collective/collective.geo.kml/branches/rockdj-ie-fixes@104091 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/geo/kml/browser/javascripts/kml_openlayers.js
+++ collective/geo/kml/browser/javascripts/kml_openlayers.js
@@ -1,56 +1,59 @@
-var thisUri = window.location.href;
-thisUri = thisUri.slice(0, thisUri.length - window.location.hash.length);
+function geo_kml() {
+	var thisUri = window.location.href;
+	thisUri = thisUri.slice(0, thisUri.length - window.location.hash.length);
 
-viewInUse = thisUri.match('@?@?kml-openlayers\/?$');
-if (viewInUse && viewInUse.length == 1) {
-      thisUri = thisUri.replace(viewInUse[0], "");
+	viewInUse = thisUri.match('@?@?kml-openlayers\/?$');
+	if (viewInUse && viewInUse.length == 1) {
+	      thisUri = thisUri.replace(viewInUse[0], "");
+	}
+
+	if (!thisUri.match('\/$')) {
+	      thisUri += '/';
+	}
+
+	kml = new OpenLayers.Layer.GML("KML Layer", thisUri + "@@kml-document",
+	{
+	    format: OpenLayers.Format.KML, 
+	    projection: map.displayProjection,
+	    formatOptions: {
+		extractStyles: true, 
+		extractAttributes: true
+	    }
+	})
+
+	map.addLayer(kml);
+	var select = new OpenLayers.Control.SelectFeature(kml);
+	    
+	kml.events.on({
+	    "featureselected": onFeatureSelect,
+	    "featureunselected": onFeatureUnselect
+	});
+
+	map.addControl(select);
+	select.activate();
+
+	function onPopupClose(evt) {
+	    select.unselectAll();
+	}
+	function onFeatureSelect(event) {
+	    var feature = event.feature;
+	    var selectedFeature = feature;
+	    var popup = new OpenLayers.Popup.FramedCloud("chicken", 
+		feature.geometry.getBounds().getCenterLonLat(),
+		new OpenLayers.Size(200,200),
+		"<h2>"+feature.attributes.name + "</h2>" + feature.attributes.description,
+		null, true, onPopupClose
+	    );
+	    feature.popup = popup;
+	    map.addPopup(popup);
+	}
+	function onFeatureUnselect(event) {
+	    var feature = event.feature;
+	    if(feature.popup) {
+		map.removePopup(feature.popup);
+		feature.popup.destroy();
+		delete feature.popup;
+	    }
+	}
 }
 
-if (!thisUri.match('\/$')) {
-      thisUri += '/';
-}
-
-kml = new OpenLayers.Layer.GML("KML Layer", thisUri + "@@kml-document",
-{
-    format: OpenLayers.Format.KML, 
-    projection: map.displayProjection,
-    formatOptions: {
-        extractStyles: true, 
-        extractAttributes: true
-    }
-})
-
-map.addLayer(kml);
-var select = new OpenLayers.Control.SelectFeature(kml);
-    
-kml.events.on({
-    "featureselected": onFeatureSelect,
-    "featureunselected": onFeatureUnselect
-});
-
-map.addControl(select);
-select.activate();
-
-function onPopupClose(evt) {
-    select.unselectAll();
-}
-function onFeatureSelect(event) {
-    var feature = event.feature;
-    var selectedFeature = feature;
-    var popup = new OpenLayers.Popup.FramedCloud("chicken", 
-        feature.geometry.getBounds().getCenterLonLat(),
-        new OpenLayers.Size(200,200),
-        "<h2>"+feature.attributes.name + "</h2>" + feature.attributes.description,
-        null, true, onPopupClose
-    );
-    feature.popup = popup;
-    map.addPopup(popup);
-}
-function onFeatureUnselect(event) {
-    var feature = event.feature;
-    if(feature.popup) {
-        map.removePopup(feature.popup);
-        feature.popup.destroy();
-        delete feature.popup;
-    }
-}
