3e59b11c61502c8c5d846cedf9e2494d9446f73f
Giorgio Borelli <giorgio@giorgioborelli.it>
2011-05-15 12:51:34 +0000

    changed kmlviewlet render method to allow users to change the position of the map in the page
    git-svn-id: http://svn.plone.org/svn/collective/collective.geo.kml/trunk@239379 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/geo/kml/browser/viewlets.py
+++ collective/geo/kml/browser/viewlets.py
@@ -1,8 +1,17 @@
 from zope.interface import implements
+from zope.component import getUtility
+
 from plone.app.layout.viewlets import ViewletBase
+from plone.registry.interfaces import IRegistry
+
 from collective.geo.geographer.interfaces import IGeoreferenced
 
+from collective.geo.settings.interfaces import (
+                                        IGeoCustomFeatureStyle,
+                                        IGeoFeatureStyle)
+
 from collective.geo.mapwidget.browser.widget import MapLayers
+
 
 from collective.geo.kml.browser.maplayers import KMLMapLayer as BaseLayer
 from collective.geo.kml.interfaces import IKMLOpenLayersViewlet
@@ -19,7 +28,20 @@ class ContentViewlet(ViewletBase):
     def coordinates(self):
         return IGeoreferenced(self.context)
 
+    @property
+    def map_viewlet_position(self):
+        custom_styles = IGeoCustomFeatureStyle(self.context)
+        if custom_styles.use_custom_styles:
+            return custom_styles.map_viewlet_position
+        else:
+            defaultstyles = getUtility(IRegistry).forInterface(
+                                                    IGeoFeatureStyle)
+            return defaultstyles.map_viewlet_position
+
     def render(self):
+        if self.manager.__name__ != self.map_viewlet_position:
+            return ''
+
         coords = self.coordinates
         if coords.type and coords.coordinates:
             return super(ContentViewlet, self).render()
