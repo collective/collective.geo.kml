574a6735d35c852658f70221b2ad3208cbb1bee0
Giorgio Borelli <casaborelli@gmail.com>
2013-05-02 23:59:09 +0200

    Changed javascript for working with new collectivegeo.js

--- collective/geo/kml/browser/javascripts/kml_openlayers.js
+++ collective/geo/kml/browser/javascripts/kml_openlayers.js
@@ -1,36 +1,43 @@
-jq(window).bind('map-load', function() {
-    var map = cgmap.config['default-cgmap'].map;
+/*global window, jQuery, document, OpenLayers*/
 
-    var kmls = map.getLayersByClass('OpenLayers.Layer.Vector');
-    var select = new OpenLayers.Control.SelectFeature(kmls);
-    select.onSelect = onFeatureSelect;
-    select.onUnselect = onFeatureUnselect;
+(function ($) {
+    "use strict";
+    $(window).bind('mapload', function (evt, widget) {
+        var map = widget.map,
+            kmls = map.getLayersByClass('OpenLayers.Layer.Vector'),
+            select = new OpenLayers.Control.SelectFeature(kmls);
 
-    map.addControl(select);
-    select.activate();
-
-    function onPopupClose(evt) {
-        select.unselectAll();
-    }
-
-    function onFeatureSelect(feature, arg1, arg2) {
-        var popup = new OpenLayers.Popup.FramedCloud(
-            "chicken",
-            feature.geometry.getBounds().getCenterLonLat(),
-            new OpenLayers.Size(200,200),
-            "<h2>"+feature.attributes.name + "</h2>" + feature.attributes.description,
-            null, true, onPopupClose
-        );
-        feature.popup = popup;
-        map.addPopup(popup);
-    }
-
-    function onFeatureUnselect(feature, arg1, arg2) {
-        if(feature.popup) {
-            map.removePopup(feature.popup);
-            feature.popup.destroy();
-            delete feature.popup;
+        function onPopupClose(evt) {
+            select.unselectAll();
         }
-    }
 
-});
+        function onFeatureSelect(feature, arg1, arg2) {
+            var popup = new OpenLayers.Popup.FramedCloud(
+                "chicken",
+                feature.geometry.getBounds().getCenterLonLat(),
+                new OpenLayers.Size(200, 200),
+                "<h2>" + feature.attributes.name + "</h2>" + feature.attributes.description,
+                null,
+                true,
+                onPopupClose
+            );
+            feature.popup = popup;
+            map.addPopup(popup);
+        }
+
+        function onFeatureUnselect(feature, arg1, arg2) {
+            if (feature.popup) {
+                map.removePopup(feature.popup);
+                feature.popup.destroy();
+                delete feature.popup;
+            }
+        }
+
+        select.onSelect = onFeatureSelect;
+        select.onUnselect = onFeatureUnselect;
+
+        map.addControl(select);
+        select.activate();
+
+    });
+}(jQuery));
